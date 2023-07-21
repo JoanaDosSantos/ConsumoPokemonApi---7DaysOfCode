6e0df41f239b1fda139e6330376438919b89df23
Joana Santos <joanasdu2014@hotmail.com>
2023-07-21 16:57:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumoPokemonApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using System;
+
+namespace Program.cs // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvocarGet();
+        }
+
+        public static void InvocarGet()
+        {
+            string idPokemon = "1";
+
+            //var client = new RestClient($"https://pokeapi.co/api/v2/pokemon-form/{idPokemon}/");
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon-form/");
+            RestRequest request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK) {
+                Console.WriteLine(response.Content);
+            } else {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            Console.ReadKey();
+        }
+    }
+}
